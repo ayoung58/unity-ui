c65823c5e3dfc9c94d9b1b9b0a9bc2274172f206
Theinterna <yalvin58@gmail.com>
2024-07-28 22:58:09 -0700

    Add and display score when targets are clicked
    Skull gets negative points

--- Unity UI/Assets/Scripts/GameManager.cs
+++ Unity UI/Assets/Scripts/GameManager.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public float timeBetweenTargets = 1;
+    public TextMeshProUGUI scoreText;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTargets());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpdateScore(int scoreToAdd) {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 
     IEnumerator SpawnTargets() {
--- Unity UI/Assets/Scripts/Target.cs
+++ Unity UI/Assets/Scripts/Target.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+    // This will be set individually in inspector based on the prefab
+    public int pointValue;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -40,6 +44,7 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown() {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other) {
