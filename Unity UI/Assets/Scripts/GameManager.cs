83f1cceb6fa23c99972fa00b5abc9fb88756b72c
Theinterna <yalvin58@gmail.com>
2024-07-28 21:43:40 -0700

    Spawn targets at a given interval of time

--- Unity UI/Assets/Scripts/GameManager.cs
+++ Unity UI/Assets/Scripts/GameManager.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public float timeBetweenTargets = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTargets());
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    IEnumerator SpawnTargets() {
+        while(true) {
+            yield return new WaitForSeconds(timeBetweenTargets);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
         
     }
 }
--- Unity UI/Assets/Scripts/Target.cs
+++ Unity UI/Assets/Scripts/Target.cs
@@ -10,6 +10,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    
     // Start is called before the first frame update
     void Start()
     {
